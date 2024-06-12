4c68d344cda21d640a4cc2f3f4a2a65c9c90797e
aareyad <aliakbar.reyad@gmail.com>
2024-06-12 16:34:01 +0600

    added marketplace addon compatibility

--- classified-listing/listing/user-information.php
+++ classified-listing/listing/user-information.php
@@ -10,29 +10,30 @@
 use Rtcl\Models\Listing;
 
 ?>
-<?php do_action('rtcl_before_user_info', $listing ); ?>
+<?php do_action( 'rtcl_before_user_info', $listing ); ?>
 <div class="rtcl-listing-user-info">
     <div class="rtcl-listing-side-title">
-        <h3><?php esc_html_e("Information", 'radius-directory'); ?></h3>
+        <h3><?php esc_html_e( "Information", 'radius-directory' ); ?></h3>
     </div>
     <div class="list-group">
-	    <?php if ( $listing->can_show_user() ): ?>
+		<?php if ( $listing->can_show_user() ): ?>
             <div class="list-group-item rtin-author d-flex align-items-center">
-                <?php
-                $pp_id = absint( get_user_meta( $listing->get_owner_id(), '_rtcl_pp_id', true ) );
-                if ( $listing->can_add_user_link() ): ?>
-                    <a href="<?php echo esc_url($listing->get_the_author_url()); ?>"><?php echo( $pp_id ? wp_get_attachment_image( $pp_id, [60, 60]) : get_avatar( $listing->get_author_id(), 60 ) ); ?></a>
-                <?php else:
-	                echo( $pp_id ? wp_get_attachment_image( $pp_id, [60, 60]) : get_avatar( $listing->get_author_id(), 60 ) );
-                endif;
-                ?>
+				<?php
+				$pp_id = absint( get_user_meta( $listing->get_owner_id(), '_rtcl_pp_id', true ) );
+				if ( $listing->can_add_user_link() ): ?>
+                    <a href="<?php echo esc_url( $listing->get_the_author_url() ); ?>"><?php echo( $pp_id ? wp_get_attachment_image( $pp_id, [ 60, 60 ] )
+							: get_avatar( $listing->get_author_id(), 60 ) ); ?></a>
+				<?php else:
+					echo( $pp_id ? wp_get_attachment_image( $pp_id, [ 60, 60 ] ) : get_avatar( $listing->get_author_id(), 60 ) );
+				endif;
+				?>
                 <h4 class="rtin-name"><?php $listing->the_author(); ?></h4>
-			    <?php do_action('rtcl_after_author_meta', $listing->get_owner_id() ); ?>
+				<?php do_action( 'rtcl_after_author_meta', $listing->get_owner_id() ); ?>
             </div>
-	    <?php endif; ?>
-        <?php do_action('rtcl_listing_seller_information', $listing); ?>
+		<?php endif; ?>
+		<?php do_action( 'rtcl_listing_seller_information', $listing ); ?>
     </div>
 </div>
-<?php do_action('rtcl_after_user_info', $listing ); ?>
+<?php do_action( 'rtcl_after_user_info', $listing ); ?>
 <!-- Social Profile  -->
 <?php do_action( 'rtcl_single_listing_social_profiles' ) ?>
